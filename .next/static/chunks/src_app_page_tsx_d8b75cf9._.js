93131dd79d5ef7ab352dc77a3e5374f5fe526bd0
Ashish Singh <ashishsingh2112005@gmail.com>
2025-07-21 04:59:06 +0530

    Add .next/static/chunks/src_app_page_tsx_d8b75cf9._.js 1753054143452

---
+++ .next/static/chunks/src_app_page_tsx_d8b75cf9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d32783f8._.js",
+  "static/chunks/node_modules_f76ae3b3._.js"
+],
+    source: "dynamic"
+});
